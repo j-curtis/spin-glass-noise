bdf545c7d3a1ba9bc1c925c27c7dcc4939e8dc35
Jonathan Bernard Curtis <jon.curtis.94@gmail.com>
2025-11-27 17:30:13 +0100

    Implemented ability to run multiple replicas using same disorder in demler_tools parallel fashion.

--- project_run_method.py
+++ project_run_method.py
@@ -9,39 +9,22 @@ import glauber
 
 
 ### Compatibility with demler_tools
-def run_MC_sims(save_filename,L,nsweeps,FM,temps,replica):
+def run_sims(save_filename,Lx,Ly,nsweeps,temps,replica,J_matrix=None):
 
-	if FM:
-		J = glauber.
+	### If a specific J matrix is not given we will by default generate an FM one  
 	
-	M = int(M)
-	T = QMC.calc_temperature(dt,M)
-	sim = QMC(Ej,Ec,T,L,M)
-	sim.over_relax = True
-	sim.local_field_draw = False #True 
-	sim.shuffle_sites = True 
-	sim.set_sampling(nburn,nsample,nstep)
+	### We generate the J matrices and the nn coupling matrix 
+	if J_matrix is None:
+		J_matrix = glauber.nn_coupling(-1.,Lx,Ly)
+	
+	
+	nns = glauber.nn_indices(Lx,Ly)
 
-
-	### Now we handle the case of a hot start 
-	if hot_start_filename is not None:
-		sim.hot_start(hot_start_filename)
-		
-	sim.burn()
-	sim.sample()
-
+	
+	### Now we run the simulations for a number of sweeps for each temperature in the schedule, annealing as we go 
+	### By default this will use random initial conditions for each replica 
+	spins,times = glauber.anneal_dynamics(J_matrix,nns,nsweeps,temps)
+	
 	with open(save_filename, 'wb') as out_file:
-        	pickle.dump((sim.action_samples,sim.OP_samples,sim.vort_samples,sim.theta_samples), out_file)
+        	pickle.dump((spins,J_matrix), out_file)
         	
-        ### now some smaller suffixed files
-        ### action samples 
-	with open(save_filename+"_action_samples",'wb') as out_file:
-		pickle.dump(sim.action_samples, out_file)
-        
-        ### OP samples 
-	with open(save_filename+"_OP_samples",'wb') as out_file:
-		pickle.dump(sim.OP_samples,out_file)
-
-
-
-
