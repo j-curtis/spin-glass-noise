84942960d07545d3dccaad28cb0434720cbe0935
Jonathan Bernard Curtis <jon.curtis.94@gmail.com>
2025-12-10 15:47:03 +0100

    Method to compute local magnetic field added.

--- noise_methods.py
+++ noise_methods.py
@@ -5,31 +5,24 @@ import numpy as np
 import glauber
 
 
-### Compatibility with demler_tools
-def run_sims(save_filename,Lx,Ly,nsweeps,temps,replica,J_seed = None,start_polarized = False):
-	J_seed = int(J_seed) 
-	replica = int(replica) 
+### Computes local magnetic noise a distance d away given a stochastic realization of the spin configuration 
+def calc_local_noise(spins,ds,Lx,Ly):
+	### Computes local magnetic noise
+	### Assumes spin qubit is a distance d away in the center of the sample 
 
-	### If a specific J matrix is not given we will by default generate an FM one  
-	
-	### We generate the J matrices and the nn coupling matrix 
-	if J_seed is None:
-		J_matrix = glauber.nn_coupling(-1.,Lx,Ly)
-	else:
-		J_matrix = glauber.nn_coupling_random(-1.,0.5,Lx,Ly,J_seed)
-	
-	nns = glauber.nn_indices(Lx,Ly)
+	### The magnetization in the z direction a distance d away is given in terms of the spins as 
+	### B_z(t) = mu_0 mu_B mu_mat/(4 pi) sum_j S_j(t) (2d^2 - R_j^2)/(R_j^2 + d^2)^(3/2) 
 
-	
-	### Now we run the simulations for a number of sweeps for each temperature in the schedule, annealing as we go 
-	### By default this will use random initial conditions for each replica 
-	if start_polarized:
-		polarized = glauber.initialize_spins(Lx,Ly)
-		spins,times = glauber.anneal_dynamics(J_matrix,nns,nsweeps,temps,initial_spins=polarized)
-		
-	else:
-		spins,times = glauber.anneal_dynamics(J_matrix,nns,nsweeps,temps)
-	
-	with open(save_filename, 'wb') as out_file:
-        	pickle.dump((spins,J_matrix), out_file) ### We store the output spin trajectory, the annealing schedule, and the J config
-        	
+	X,Y = np.meshgrid(np.arange(Lx)-Lx//2, np.arange(Ly)-Ly//2,indexing='ij')
+
+	R2 = np.sqrt(X**2 + Y**2) 
+
+	R2 = R2.ravel() 
+
+	kernels = (2.*ds[None,:]**2 - R2[:,None])/( R2[:,None] + ds[None,:]**2)**1.5 ### Has shape [Nspins, Nds] 
+
+	noise = np.tensordot(spins,kernels,axes=[-2,0])
+
+	return noise 
+    
+    
