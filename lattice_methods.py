abb5c752f9a724bf6e4f6fe9e9ee7f8543696e2c
Jonathan Curtis <jon.curtis.94@gmail.com>
2026-06-17 13:48:25 +0200

    Patched issue in construction of lattice for nnn where symmetrization was ruining the intended distribution of nnn coupling values.

--- lattice_methods.py
+++ lattice_methods.py
@@ -8,6 +8,8 @@ class lattice:
 	
 	def __init__(self,L):
 		### Generates an LxL square lattice 
+		self.symmetry_tol = 1.e-7 ### Default tolerance for checking the coupling matrix is symmetric 
+
 		self.L = L 
 		self.N = L*L
 		self.sites = np.arange(self.N) 
@@ -17,9 +19,10 @@ class lattice:
 		self.nnns = []
 		
 		### General list of interaction pairs 
+		### !!! In future should make this list built in the setter methods so that it can be made variable size depending on distance of neighbors included 
 		self.partners = [] 
 		
-		self.J_matrix = np.zeros((self.N,self.N)) 
+		self.J_matrix = np.zeros((self.N,self.N))
 		
 		for i in self.sites:
 			r = self.index_to_coordinate(i) 
@@ -45,9 +48,10 @@ class lattice:
 				
 			self.nns.append(nns_site)
 			self.nnns.append(nnns_site)
+			self.partners.append(partners_site)
 			
 		self.seed = None 
-		self.rng = None
+		self.rng = np.random.default_rng()
 
 	### Flattened index to a coordinate in real space 
 	def index_to_coordinate(self,i):
@@ -74,14 +78,15 @@ class lattice:
 		for i in self.sites:
 			 
 			for j in self.nns[i]:
-				self.J_matrix[j,i] = self.rng.choice([self.Jnn, -self.Jnn],p=[self.pnn,1.-self.pnn])
-			
-			### We also add the partners to the list 
-			self.partners.append(self.nns[i])
-			
-		### This needs to be symmetric in the end 
-		self.J_matrix = 0.5*(self.J_matrix + np.transpose(self.J_matrix))
+				if j < i:
+					Jij = self.rng.choice([self.Jnn, -self.Jnn],p=[self.pnn,1.-self.pnn]) ### This is the random value
+					### To avoid double counting we will only fill if j < i but fill both J[j,i] and J[i,j]
+					self.J_matrix[j,i] = Jij
+					self.J_matrix[i,j] = Jij
+
+		return self.check_symmetric()
 		
+
 	### This will set nearest neighbor coupling to be a Gaussian distribution 
 	def set_nn_Gaussian(self,Javg,Jstd):
 		self.Jnn_avg = Javg
@@ -89,13 +94,13 @@ class lattice:
 		
 		for i in self.sites:
 			for j in self.nns[i]:
-				self.J_matrix[j,i] = self.rng.normal(self.Jnn_avg,self.Jnn_std) 
-				
-			self.partners.append(self.nns[i])
-			
-		self.J_matrix = 0.5*(self.J_matrix + np.transpose(self.J_matrix)) 
-		
-		
+				if j < i:
+					Jij = self.rng.normal(self.Jnn_avg,self.Jnn_std) 
+					self.J_matrix[i,j] = Jij
+					self.J_matrix[j,i] = Jij
+
+		return self.check_symmetric()
+
 	### Sets the next-nearest-neighbor couplings with optional choice between value and 0 
 	def set_nnn_J(self,Jnnn,p=1.):
 		self.Jnnn = Jnnn
@@ -103,13 +108,21 @@ class lattice:
 	
 		for i in self.sites:
 			for j in self.nnns[i]:
-				self.J_matrix[j,i] = self.rng.choice([self.Jnnn, 0.],p=[self.pnnn,1.-self.pnnn])
-				
-			### Add the partners to the list
-			self.partners.append(self.nnns[i])
-			
-		### This needs to be symmetric in the end 
-		self.J_matrix = 0.5*(self.J_matrix + np.transpose(self.J_matrix))
+				if j < i:
+					Jij = self.rng.choice([self.Jnnn, 0.],p=[self.pnnn,1.-self.pnnn])
+					self.J_matrix[j,i] = Jij
+					self.J_matrix[i,j] = Jij
+							
+		return self.check_symmetric()
+
+	### Check that the coupling matrix is symmetric 
+	def check_symmetric(self):
+		max_asymmetry = np.max( np.abs( self.J_matrix - self.J_matrix.T ) )
+
+		if max_asymmetry > self.symmetry_tol:
+			raise ValueError(f"Coupling matrix is not symmetric: max asymmetry = {max_asymmetry}")
+
+		return True
 		
 		
 		
